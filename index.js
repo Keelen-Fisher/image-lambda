ca22b62e6f766e1116eb03b38daddd79547dd9e0
Keelen Fisher <keelencarrera@gmail.com>
2022-10-12 14:05:27 -0500

    modified code

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 'use strict';
 
 // ------------------------------------------------------------------------------Code used to Test:------------------------------------------------------------------------------------------------------------
-// Credit to Demo Code and Luis Rosales
+// Credit Help from  Luis Rosales
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 exports.handler = async (event) => {
-    let bucketName = event.Records[0].s3.bucket.name;
+    let nameWork = event.Records[0].s3.bucket.name;
     let key = event.Records[0].s3.object.key;
     let size = event.Records[0].s3.object.size;
     try {
-    let images = await S3.getObject({Bucket: bucketName, Key: key}).promise();
+    let images = await S3.getObject({Bucket: nameWork, Key: key}).promise();
     let stringifiedImages = images.Body.toString();
-    let parsedImages = JSON.parse(stringifiedImages);
-    parsedImages.push({
+    let imagesParsedToChange = JSON.parse(stringifiedImages);
+    imagesParsedToChange.push({
         name: key,
         size: size,
         type: "jpg"
     });
      const params = {
-        Bucket: bucketName,
+        Bucket: nameWork,
         Key: "images.json"
     };
-    let imgData = JSON.stringify(parsedImages);
+    let imgData = JSON.stringify(imagesParsedToChange);
     await S3.putObject({... params, Body: imgData}).promise();
-    console.log('parsed images ----------', parsedImages);
+    console.log('parsed images ----------', imagesParsedToChange);
     } catch (e) {
         console.log(e);
-        let newImage = {
-            Bucket: bucketName,
+        let uploadImage = {
+            Bucket: nameWork,
             Key: "images.json",
             Body: JSON.stringify([{name: key, size: size, type: ".jpg"}]),
         };
-        await S3.putObject(newImage).promise();
+        await S3.putObject(uploadImage).promise();
     }
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Testing Testing'),
     };
     return response;
 };
